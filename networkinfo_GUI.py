913bf1873519b3d175122173b9e9b3402cd9f924
Yash Dabke <112268819+yashdabke@users.noreply.github.com>
2023-09-06 16:11:19 +0530

    Update networkinfo_GUI.py

--- networkinfo_GUI.py
+++ networkinfo_GUI.py
@@ -1,9 +1,11 @@
-# pip install speedtest only for this code - provides a gui window to display network details 
+# pip install speedtest and speedtest-cli libs 
+#code includes gui with progress bar to show loading process while retrieving network information
 
 import subprocess
 import socket
 import tkinter as tk
 from tkinter import messagebox
+from tkinter import ttk
 
 def get_wifi_profiles():
     try:
@@ -16,8 +18,8 @@ def get_wifi_profiles():
         # Extract profile names
         profiles = [i.split(":")[1][1:-1] for i in data if "All User Profile" in i]
         return profiles
-    except subprocess.CalledProcessError:
-        print("Error fetching WiFi profiles.")
+    except subprocess.CalledProcessError as e:
+        messagebox.showerror("Error", f"Error fetching WiFi profiles: {str(e)}")
         return []
 
 def get_wifi_password(profile_name):
@@ -35,7 +37,8 @@ def get_wifi_password(profile_name):
             return password[0]
         else:
             return "No password found"
-    except subprocess.CalledProcessError:
+    except subprocess.CalledProcessError as e:
+        messagebox.showerror("Error", f"Error fetching password: {str(e)}")
         return "Error fetching password"
 
 def get_ip_address():
@@ -46,21 +49,6 @@ def get_ip_address():
         return ip_address
     except socket.error:
         return "Error fetching IP address"
-
-def get_available_networks():
-    try:
-        # Use 'netsh' command to retrieve available WiFi networks
-        networks = (
-            subprocess.check_output(["netsh", "wlan", "show", "network"])
-            .decode("utf-8")
-            .split("\n")
-        )
-        # Extract SSID (network names)
-        available_networks = [line.strip() for line in networks if "SSID" in line]
-        return available_networks
-    except subprocess.CalledProcessError:
-        print("Error fetching available networks.")
-        return []
 
 def test_network_speed():
     try:
@@ -81,6 +69,21 @@ def test_network_speed():
     except subprocess.CalledProcessError:
         return "Error executing speedtest-cli"
 
+def get_available_networks():
+    try:
+        # Use 'netsh' command to retrieve available WiFi networks
+        networks = (
+            subprocess.check_output(["netsh", "wlan", "show", "network"])
+            .decode("utf-8")
+            .split("\n")
+        )
+        # Extract SSID (network names)
+        available_networks = [line.strip() for line in networks if "SSID" in line]
+        return available_networks
+    except subprocess.CalledProcessError as e:
+        messagebox.showerror("Error", f"Error fetching available networks: {str(e)}")
+        return []
+
 def save_to_file(data, filename):
     with open(filename, 'w') as file:
         # Write data to a text file
@@ -88,28 +91,37 @@ def save_to_file(data, filename):
             file.write(f"{item}\n")
 
 def fetch_data_and_save():
-    # Fetch WiFi profiles, IP address, available networks, and network speed
-    wifi_profiles = get_wifi_profiles()
-    ip_address = get_ip_address()
-    available_networks = get_available_networks()
-    speed = test_network_speed()  # Get the network speed as a single value
+    try:
+        progress_bar.start()  # Start the progress bar
+        fetch_button.config(state=tk.DISABLED)  # Disable the fetch button during data retrieval
 
-    # Update labels with fetched information
-    wifi_profiles_label.config(text="WiFi Profiles:\n" + "\n".join(wifi_profiles))
-    ip_address_label.config(text="IP Address:\n" + ip_address)
-    available_networks_label.config(text="Available Networks:\n" + "\n".join(available_networks))
-    network_speed_label.config(text=f"Network Speed:\nDownload Speed: {speed} Mbps")
+        wifi_profiles = get_wifi_profiles()
+        ip_address = get_ip_address()
+        available_networks = get_available_networks()
+        speed = test_network_speed()  # Get the network speed as a single value
 
-    # Save information to text files
-    save_to_file(wifi_profiles, 'wifi_profiles.txt')
-    save_to_file([ip_address], 'ip_address.txt')
-    save_to_file(available_networks, 'available_networks.txt')
-    save_to_file([f"Download Speed: {speed} Mbps"], 'network_speed.txt')
+        # Update labels with fetched information
+        wifi_profiles_label.config(text="WiFi Profiles:\n" + "\n".join(wifi_profiles))
+        ip_address_label.config(text="IP Address:\n" + ip_address)
+        available_networks_label.config(text="Available Networks:\n" + "\n".join(available_networks))
+        network_speed_label.config(text=f"Network Speed:\nDownload Speed: {speed} Mbps")
 
-    messagebox.showinfo("Success", "Data has been fetched and saved successfully!")
+        # Save information to text files
+        save_to_file(wifi_profiles, 'wifi_profiles.txt')
+        save_to_file([ip_address], 'ip_address.txt')
+        save_to_file(available_networks, 'available_networks.txt')
+        save_to_file([f"Download Speed: {speed} Mbps"], 'network_speed.txt')
+
+        messagebox.showinfo("Success", "Data has been fetched and saved successfully!")
+    except Exception as e:
+        messagebox.showerror("Error", f"An error occurred: {str(e)}")
+    finally:
+        progress_bar.stop()  # Stop the progress bar
+        fetch_button.config(state=tk.NORMAL)  # Enable the fetch button after data retrieval
 
 def main():
     global wifi_profiles_label, ip_address_label, available_networks_label, network_speed_label
+    global fetch_button, progress_bar
 
     window = tk.Tk()
     window.title("Network Info Tool")
@@ -117,6 +129,10 @@ def main():
     # Create a button to fetch data and save
     fetch_button = tk.Button(window, text="Fetch Data and Save", command=fetch_data_and_save)
     fetch_button.pack()
+
+    # Create a progress bar
+    progress_bar = ttk.Progressbar(window, mode='indeterminate')
+    progress_bar.pack()
 
     # Create labels to display fetched information
     wifi_profiles_label = tk.Label(window, text="WiFi Profiles:")
@@ -135,4 +151,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-    
